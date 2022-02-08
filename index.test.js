48c884815acaa4b821a6c123f3db12f1f1585653
Lukas Koeller <acc.koeller@protonmail.com>
2022-02-08 10:37:06 +0100

    test: add two basic tests

--- index.test.js
+++ index.test.js
@@ -3,15 +3,24 @@ const postcss = require('postcss')
 const plugin = require('./')
 
 async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
+  let result = await postcss([plugin(opts)]).process(input, { from: '/test.css' })
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
 
-/* Write tests here
-
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
-})
-
-*/
+describe('CSS Flexbox Gap Polyfill', () => {
+  it('should fallback when gap and display: flex are defined in a single selector', async () => {
+    run(
+      '.some { color: red; } .single { --gap: 24px; display: flex; gap: var(--gap); }',
+      '.some { color: red; } .single { --gap: 24px; display: flex; gap: var(--gap);--pfg-gap: var(--gap); } .single > * + * { margin-left: var(--pfg-gap); }',
+      { }
+    )
+  });
+  it('should fallback when gap and display: flex are defined in multiple selectors', () => {
+    run(
+      '.some { color: red; } .first { --gap: 24px; gap: var(--gap); } .second { display: flex; }',
+      '.some { color: red; } .first { --gap: 24px; gap: var(--gap);--pfg-gap: var(--gap); } .second { display: flex; } .second > * + * { margin-left: var(--pfg-gap); }',
+      { }
+    )
+  });
+});
