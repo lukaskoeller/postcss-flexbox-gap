692e9abc22e797773888e124a4015001e5940c55
Lukas Koeller <acc.koeller@protonmail.com>
2022-02-08 11:58:21 +0100

    feat: support further gap properties

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const DISPLAY_FLEX_VALUES = ['flex', 'inline-flex'];
+// row-gap is not mentioned here since this plugin only supports single line usage of flexbox.
+const GAP_VALUES = ['gap', 'column-gap', 'grid-gap', 'grid-column-gap'];
+const [GAP, COLUMN_GAP, GRID_GAP, GRID_COLUMN_GAP] = GAP_VALUES;
 const CUSTOM_GAP_PROPERTY = '--pfg-gap';
+
+/**
+ * Adds a new custom gap declaration using the gap value
+ * and removes the gap declaration.
+ * @param {*} decl 
+ */
+const modifyGapProp = (decl) => {
+  decl.after(`${CUSTOM_GAP_PROPERTY}: ${decl.value}`);
+  decl.remove();
+};
 
 // Help: https://github.com/postcss/postcss/blob/main/docs/writing-a-plugin.md
 // Guidelines: https://github.com/postcss/postcss/blob/main/docs/guidelines/plugin.md
@@ -25,10 +38,11 @@ module.exports = (/* opts = {} */) => {
     */
 
     Declaration: {
-      gap: (decl) => {
-        decl.after(`${CUSTOM_GAP_PROPERTY}: ${decl.value}`);
-        decl.remove();
-      },
+      // ...Object.fromEntries(GAP_VALUES.map((value) => [value, modifyGapProp])),
+      [GAP]: modifyGapProp,
+      [COLUMN_GAP]: modifyGapProp,
+      [GRID_GAP]: modifyGapProp,
+      [GRID_COLUMN_GAP]: modifyGapProp,
       display: (decl) => {
         const isFlexContainer = DISPLAY_FLEX_VALUES.includes(decl.value);
 
